58823a534c88a3beb0ae5024678cf45a68c466ef
skv0r <skvorgrand@gmail.com>
2025-05-04 23:03:50 +0300

    404page add

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[62vh]">
+        <h1 className="text-4xl font-bold">404</h1>
+        <p className="text-2xl">Страница не найдена</p>
+    </div>
+  );
+};
+
+export default NotFound;
+

2d316833283747eb0166a34a41c0a67cb46754d7
skv0r <skvorgrand@gmail.com>
2025-05-04 19:53:46 +0300

    pagination catalog

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -3,7 +3,7 @@ import Filter from "@/shared/ui/catalog/Filter/Filter";
 
 const Catalog = () => {
   return (
-    <div className="py-8 px-[52px]" >
+    <div className="py-8 px-[52px] border-1 border-[#CBD5E1] rounded-md" >
         <h1 className="text-[48px] font-bold pb-4">Каталог</h1>
         <div className="flex justify-between">
             <Filter />

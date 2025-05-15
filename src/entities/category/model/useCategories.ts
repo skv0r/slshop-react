dc7da10b72df54388af4537aad76bdb6d34f0551
skv0r <skvorgrand@gmail.com>
2025-05-15 16:27:28 +0300

    useCategories rdy

---
+++ src/entities/category/model/useCategories.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import Category from "./types";
+
+const useCategory = () => {
+    const [catergories, setCategories] = useState<Category[]>([]);
+    const [isLooading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect( () => {
+        const loadCategories = async () => {
+            setIsLoading(true)
+            setError(null)
+
+            try {
+                const data = await fetchCategories()
+                setCategories(data)
+            } catch (err) {
+                setError(err instanceof Error ? err.message : "Ошибка в fetch categories")
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+        loadCategories()
+    }, [])
+    return { catergories, isLooading, error}
+}
+
+export default useCategory;
